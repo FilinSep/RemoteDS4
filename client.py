cf05f08f93fca6a678c6f75376547fd6151580a1
FilinSep <telsa6993@gmail.com>
2025-05-19 19:47:58 +0300

    first commit

---
+++ client.py
@@ -0,0 +1,83 @@
+import colorama
+colorama.init()
+from colorama import Fore
+
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+import pygame
+pygame.init()
+
+import socket
+import json
+import time
+
+
+def join():
+    print(Fore.LIGHTGREEN_EX + 'RemoteDS4')
+    print(Fore.RESET + 'GitHub: https://github.com/FilinSep')
+    print()
+    print(Fore.YELLOW + '*' + Fore.RESET + ' Allows you to play local co-op games on the DualShock4 joystick as a multiplayer using python sockets. Also you can use RadminVPN.')
+    print()
+
+    IP = input('ip > ' + Fore.YELLOW)
+    print(Fore.RESET, end='')
+    PORT = input('port > ' + Fore.YELLOW)
+    print(Fore.RESET, end='')
+    os.system('cls')
+    
+    sock = socket.socket()
+
+    try:
+        print('Connecting...')
+        sock.connect((IP, int(PORT)))
+    except:
+        print('Wrong address')
+        exit()
+
+    os.system(f'title Party on {IP}:{PORT}')
+
+    while True:
+        try:
+            j = pygame.joystick.Joystick(0)
+            j.init()
+
+            os.system('cls')
+
+            while True:
+                time.sleep(0.005)
+                events = pygame.event.get()
+                
+                axis = {
+                    't': 'axis',
+                    'l3_x': j.get_axis(0),
+                    'l3_y': j.get_axis(1),
+                    'r3_x': j.get_axis(2),
+                    'r3_y': j.get_axis(3),
+                    'l2_a': j.get_axis(4),
+                    'r2_a': j.get_axis(5)
+                }
+
+                sock.send(json.dumps(axis).encode())
+
+                for event in events:
+                    if event.type == pygame.JOYBUTTONDOWN:
+                        print(f'INPUT DOWN {event.button}')
+                        sock.send(json.dumps({'t': 'down', 'key': event.button}).encode())
+                    elif event.type == pygame.JOYBUTTONUP:
+                        print(f'INPUT UP {event.button}')
+                        sock.send(json.dumps({'t': 'up', 'key': event.button}).encode())
+                    elif event.type == pygame.JOYAXISMOTION and event.axis == 4:
+                        print(f'INPUT L2 [{event.value}]')
+                    elif event.type == pygame.JOYAXISMOTION and event.axis == 5:
+                        print(f'INPUT R2 [{event.value}]')
+        except socket.error:
+            print('Party ended :(')
+            input()
+            exit()
+        except Exception as e:
+            print('Waiting for DualShock4...', end='\r')
+ 
+
+if __name__ == "__main__":
+    join()
---
+++ server.py
@@ -0,0 +1,87 @@
+import colorama
+colorama.init()
+from colorama import Fore
+
+import json
+import socket
+import os
+import re
+
+import vgamepad
+from keymaps import keymaps
+
+
+ds4 = vgamepad.VDS4Gamepad()
+
+
+def make():
+    os.system('cls')
+
+    HOST = "127.0.0.1"
+    PORT = 5565
+    
+    print('Created virtual DS4 gamepad. Number:' + Fore.YELLOW, ds4.get_index() + 1, Fore.RESET)
+    print('Hosting server on ' + Fore.YELLOW + f'{HOST}:{PORT}' + Fore.RESET)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by ' + Fore.YELLOW + f'{addr[0]}' + Fore.RESET)
+            while True:
+                try: 
+                    r_data = conn.recv(1024)
+                    redata = re.match(r'\{.*\}', r_data.decode())
+                    
+                    groups = []
+                    if not redata.groups():
+                        groups.append(redata[0])
+
+                    for data in groups:
+                        data = json.loads(data)
+
+                        if data['t'] == 'down':
+                            if keymaps[data['key']].__class__ == vgamepad.DS4_SPECIAL_BUTTONS:
+                                ds4.press_special_button(keymaps[data['key']])
+                            elif keymaps[data['key']].__class__ == vgamepad.DS4_DPAD_DIRECTIONS:
+                                ds4.directional_pad(keymaps[data['key']])
+                            else:
+                                ds4.press_button(keymaps[data['key']])
+
+                            ds4.update()
+                            
+                        elif data['t'] == 'up':
+                            if keymaps[data['key']].__class__ == vgamepad.DS4_SPECIAL_BUTTONS:
+                                ds4.release_special_button(keymaps[data['key']])
+                            elif keymaps[data['key']].__class__ == vgamepad.DS4_DPAD_DIRECTIONS:
+                                ds4.directional_pad(vgamepad.DS4_DPAD_DIRECTIONS.DS4_BUTTON_DPAD_NONE)
+                            else:
+                                ds4.release_button(keymaps[data['key']])
+
+                            ds4.update()
+
+                            print('OUTPUT ' + Fore.YELLOW + f'{keymaps[data['key']].name}', Fore.RESET)
+
+                        elif data['t'] == 'axis':
+                            ds4.left_trigger_float(data['l2_a'])
+                            ds4.update()
+                            ds4.right_trigger_float(data['r2_a'])
+                            ds4.update()
+
+                            ds4.left_joystick_float(data['l3_x'], data['l3_y'])
+                            ds4.update()
+                            ds4.right_joystick_float(data['r3_x'], data['r3_y'])
+                            ds4.update()
+                    
+                except Exception as e: 
+                    print('Party ended :(', e)
+                    input()
+                    break
+
+                if not r_data:
+                    pass
+
+
+if __name__ == "__main__":
+    make()
